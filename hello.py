89fecc982caa8339ab709bd0143fe0c1186fef7c
bluepoet <bluepoet1112@gmail.com>
2015-10-18 17:41:55 +0900

    ! added

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 print("Hello world")
 print("Tell Your World")
-print("Tell his World")
-print("Tell her World")
+print("Tell his World!")
+print("Tell her World!")
 print("Tell my World")
 print("Tell mine World")
 
