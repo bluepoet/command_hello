9c52118e0bebf6bac9a0a96e2281ee1546f5069f
YongHoon KIM <bluepoet1112@gmail.com>
2015-10-18 17:41:28 +0900

    added test

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ print("Hello world")
 print("Tell Your World")
 print("Tell his World")
 print("Tell her World")
-print("Tell my World")
-print("Tell mine World")
+print("Tell my World!!")
+print("Tell mine World!!")
 
